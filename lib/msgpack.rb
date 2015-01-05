87c46a108dded7ed82a0833a34420d5e290437d5
TAGOMORI Satoshi <tagomoris@gmail.com>
2015-01-05 16:19:33 +0900

    add comments to describe what these are

--- lib/msgpack.rb
+++ lib/msgpack.rb
@@ -4,9 +4,9 @@ if /java/ =~ RUBY_PLATFORM
   $LOAD_PATH << File.expand_path("../ext", __FILE__)
   require 'java'
   require 'javassist-3.15.0-GA'
-  require 'msgpack-0.6.6'
-  require 'msgpack_jruby'
-  require 'msgpack/jruby_support'
+  require 'msgpack-0.6.6' # messagepack java jar
+  require 'msgpack_jruby' # messagepack jruby
+  require 'msgpack/jruby_support' # to add #to_msgpack methods on built-in classes
 else
   begin
     require "msgpack/#{RUBY_VERSION[/\d+.\d+/]}/msgpack"
